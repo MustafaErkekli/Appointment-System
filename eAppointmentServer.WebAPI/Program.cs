e312e911d22fb43618bd5811cb801c177e0a067a
MustafaErkekli <emustafaerkekli.ee@gmail.com>
2024-10-07 22:17:48 +0300

    Add project files.

---
+++ eAppointmentServer.WebAPI/Program.cs
@@ -0,0 +1,38 @@
+using DefaultCorsPolicyNugetPackage;
+using eAppointmentServer.Application;
+using eAppointmentServer.Domain.Entities;
+using eAppointmentServer.Infrastructure;
+using eAppointmentServer.WebAPI;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDefaultCors();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseAuthorization();
+
+app.MapControllers();
+
+Helper.CreateUserAsync(app).Wait();
+
+app.Run();
